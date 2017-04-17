971e8c9ecf269350398ad0f9f732b40340219b89
Luis Medrano <lmedrano@multisistemas.com.sv>
2017-04-17 15:12:34 -0600

    Drop unused files

--- htdocs/plugins/gconnect/sql/llx_gconnect.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Multisistemas Gconnect - A Google authentication module for Dolibarr
- * Copyright (C) 2017 Herson Cruz <herson@multisistemas.com.sv>
- * Copyright (C) 2017 Luis Medrano <lmedrano@multisistemas.com.sv>
- *
- */
- 
